0329c84de1b332de341d6c205b4364f3a874f1c7
Adrian Abadin <aabadin@gmail.com>
2024-03-12 22:43:40 -0300

    Added mailing route validation

--- src/mailing/mailing.routes.ts
+++ src/mailing/mailing.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { mailingController } from "./mailing.controller";
+import { validateSchema } from "../middlewares/zodValidation";
+import { mailBody } from "./mailing.schemas";
 export const mailRouter = Router()
 
-mailRouter.post("/",mailingController.sendMail)
+mailRouter.post("/",validateSchema(mailBody),mailingController.sendMail)
